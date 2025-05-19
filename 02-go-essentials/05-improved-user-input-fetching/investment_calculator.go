8859d7bd5262c4698dca66173970aa1b1c341a9a
Vignesh Krishnaraj <vkrishnaraj@atlassian.com>
2025-05-19 21:31:34 +0530

    updating 2nd lesson - 05

--- 02-go-essentials/05-improved-user-input-fetching/investment_calculator.go
+++ 02-go-essentials/05-improved-user-input-fetching/investment_calculator.go
@@ -8,11 +8,18 @@ import (
 func main() {
 	fmt.Println("Hello, World!")
 	const inflationRate = 2.5
-	var investmentAmount float64 = 1000
+	var investmentAmount float64
+	var years float64
 	expectedReturnRate := 5.5
-	years := 10.0
 
+	fmt.Print("Investment Amount: ")
 	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
